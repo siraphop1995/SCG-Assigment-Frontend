34a78df13acb793b237fca2cc468cb01cfecc07b
Siraphop1995 <southeater95@gmail.com>
2019-09-28 22:36:57 +0700

    heroku testing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <img src="../assets/siraphop.png" style="width:70%" />
+    <img src="../assets/logo.png" style="width:100%" />
+
     <!-- <b-img :img="require('@/assets/Siraphop.png')" fluid alt="Responsive image" style="size:10%"></b-img>
     <b-img img="@/assets/Siraphop.png" fluid alt="Responsive image"></b-img>-->
   </div>
