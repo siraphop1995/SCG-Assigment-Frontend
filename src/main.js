bd4ec6be37a60e8f80e8e92c75f82925df8a127a
Siraphop1995 <southeater95@gmail.com>
2019-09-28 21:59:37 +0700

    build test

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ import store from './store'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://localhost:3000'
+// axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'https://agile-crag-49831.herokuapp.com'
 
 new Vue({
   router,
